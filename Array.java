9d80777aedf090c244efdd6f84cc903949d45a9a
Narendar2024 <narendarkorem471@gmail.com>
2024-05-27 22:28:20 +0530

    Fetching Array Elements using Defferint methods

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        int nums[]={2,5,4,6,7};
+
+        for(int i = 0; i < 4; i++)
+        System.out.println(nums[i]);
+    }
+}
---
+++ ThreeDimensionalArray.java
@@ -0,0 +1,33 @@
+public class ThreeDimensionalArray {
+    public static void main(String[] args) {
+        int nums[][][]= new int[3][4][5];
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<4; j++){
+                for(int k=0; k<5; k++){
+                    nums[i][j][k] = (int)(Math.random()*10);
+                    System.out.print(nums[i][j][k]+"    ");
+                }
+                System.out.println();
+            }
+        }
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<4; j++){
+                for(int k=0; k<5; k++){
+                    System.out.print(nums[i][j][k]+" ");
+                }
+                System.out.println();
+            }
+        }
+
+        for(int array2D[][]:nums){
+            for(int array1D[]:array2D){
+                for(int value:array1D){
+                    System.out.print(value+"    ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
